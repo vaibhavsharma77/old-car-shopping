582d46fe31b51542106757fc8f16ceb8b5e007cb
vaibhavsharma <vaibhavsharma777@yahoo.com>
2021-03-18 23:35:23 +0100

    intial commit

---
+++ src/main/java/com/car/shopping/carshoppingdemo/CarShoppingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.car.shopping.carshoppingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShoppingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShoppingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/car/shopping/carshoppingdemo/CarShoppingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.shopping.carshoppingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShoppingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
